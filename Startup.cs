b8000bf854ea57281e59b60a926643ab110fa108
Lyf <liu.yunfei@hotelsys.cn>
2018-06-06 10:50:15 +0800

    01、测试修改 02、暂存所有更改

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace FirstCore
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello AspNet Core!");
             });
         }
     }
